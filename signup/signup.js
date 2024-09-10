a65120f30bbc463cdd8b1f5e7724cc4f609b7e5e
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-09-11 04:11:58 +0500

    /

--- signup/signup.js
+++ signup/signup.js
@@ -2,7 +2,11 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+  import { getAnalytics,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+   } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you -want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   
@@ -19,3 +23,11 @@
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
+  const first_Name = document.getElementById("first_Name");
+  const last_Name = document.getElementById("last_Name");
+  const Email = document.getElementById("Email");
+  const Password = document.getElementById("Password");
+  const Signup_btn= document.getElementById("Signup_btn");
+  
+  Signup_btn.addEventListener("click" .createUserAccount);
+  
